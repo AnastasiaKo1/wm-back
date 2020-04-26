04861f281db74717ac0cee00c8f47b598fbb0b43
alimalimit <alimalimit@gmail.com>
2020-04-26 23:06:43 +0600

    Alima

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('articles/', views.Articles.as_view()),
-    path('articles/publish/', views.create_article),
-    path('categories/', views.categories),
-
-]
